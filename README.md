1787e4250b185a28686390bc106bcbe4f154580f
jonas-doerr <jonasdoerr2@gmail.com>
2026-07-31 11:32:38 -0500

    README + classes

---
+++ README.md
@@ -0,0 +1,6 @@
+# TICHU SCOREKEEPER
+
+This program tracks scores for the card game Tichu and stores the data from each game. It also (in the future) provides analysis on that data.
+
+### 7.31
+I created the program and set up a file structure.
--- scorekeeper.py
+++ scorekeeper.py
@@ -1,2 +1,27 @@
 class TichuGame:
+    def __init__(self):
+        self.rounds = []
+        self.score1 = 0
+        self.score2 = 0
+
+    def add_round(self, score1, score2):
+        self.score1 += score1
+        self.score2 += score2
+
+        self.rounds.append({
+            "team1": score1,
+            "team2": score2
+        })
+
+    def winner(self):
+        if self.score1 >= 1000 and self.score1 > self.score2:
+            return "Team 1"
+        elif self.score2 >= 1000 and self.score2 > self.score1:
+            return "Team 2"
+        return None
+
+class Player:
+    pass
+
+class Database:
     pass
